ea9a17953437d2a333d740dab14cc144b8b3746b
itizawa <itizawa@weseek.co.jp>
2020-03-29 14:54:22 +0900

    create api

---
+++ pages/api/admin/list.js
@@ -0,0 +1,34 @@
+import { db } from '../../../lib/db'
+
+export default async (req, res) => {
+
+  console.log(req.query)
+  const { userId } = req.query
+
+  const fetchList = async () => {
+    let ogpList = []
+    try {
+      const snapshot = await db.collection("ogp").where("creatorId", "==", userId).get()
+      await snapshot.forEach(function (doc) {
+        // doc.data() is never undefined for query doc snapshots
+        ogpList.push(Object.assign({ id: doc.id }, doc.data()))
+      });
+      res.status(200).send({ ogpList });
+    } catch (err) {
+      console.log(err)
+      throw err
+    }
+  }
+
+
+  const { method } = req
+  switch (method) {
+    case 'GET':
+      fetchList()
+      break;
+    default:
+      break;
+  }
+
+
+};
